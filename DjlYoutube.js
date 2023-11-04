c9551ee468d20ab818d90408a66aecabe3ea300a
Dylan Lom <dylanlom@gmail.com>
2023-11-04 12:13:38 +1100

    Add DjlYoutube component

---
+++ DjlYoutube.js
@@ -0,0 +1,70 @@
+'use strict';
+
+class DjlYoutube extends HTMLElement {
+  static ytApi = null; /* Populated by onYouTubeIframeAPIReady */
+  static {
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    document.body.appendChild(tag);
+
+    window.onYouTubeIframeAPIReady = function() {
+      DjlYoutube.ytApi = YT
+      Array.from(document.querySelectorAll('djl-youtube'))
+        .forEach(e => e.onYouTubeIframeAPIReady())
+    }
+  }
+  static maxElementId = 0
+
+  static get observedAttributes() {
+    return [ 'height', 'width', 'videoid' ];
+  }
+
+  get height() { return this.getAttribute('height') || 640; }
+  set height(x) { return this.setAttribute('height', x); }
+
+  get width() { return this.getAttribute('width') || 480; }
+  set width(x) { return this.setAttribute('width', x); }
+
+  get videoid() { return this.getAttribute('videoid'); }
+  set videoid(x) { return this.setAttribute('videoid', x); }
+
+  element;
+  player; // YT.player controller
+
+  constructor() {
+    super();
+
+    this.element = document.createElement('div')
+    this.element.id = `__djl_youtube_player_${DjlYoutube.maxElementId}`;
+    DjlYoutube.maxElementId += 1;
+    this.appendChild(this.element);
+
+    if (DjlYoutube.ytApi) this.onYouTubeIframeAPIReady();
+  }
+
+  onYouTubeIframeAPIReady() {
+    this.player = new DjlYoutube.ytApi.Player(this.element.id, {
+      height: this.height,
+      width: this.width,
+      videoId: this.videoid,
+      playerVars: {
+        playsinline: 1
+      },
+    });
+  }
+
+
+  attributeChangedCallback() {
+    if (!this.player) return;
+
+    console.log(this.player);
+
+    this.player.setSize(this.width, this.height);
+    if (this.player.getVideoData().video_id != this.videoid);
+      this.player.loadVideoById(this.videoid);
+
+  }
+}
+
+customElements.define('djl-youtube', DjlYoutube);
+
