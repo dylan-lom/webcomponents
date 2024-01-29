f1a79deca336a9ac431f4fe2e7f53e5f6043fdd0
Dylan Lom <dylanlom@gmail.com>
2024-01-29 15:19:49 +1100

    Use lifecycle callbacks and attribute get-/set-ers in DjlLipsum

--- DjlLipsum.js
+++ DjlLipsum.js
@@ -1,43 +1,46 @@
 /**
  * 
- * @property {string} baseText           - Text that textContent will be based on.
- * @property {number | undefined} length - Length of text to be displayed (in characters). If wrap is false, this is inferred as maximum length.
- * @property {boolean} randomStart       - Whether baseText should pick a random word to start on.
- * @property {boolean} wrap              - Whether baseText should return to the start if the length
+ * @attribute {number | undefined} length - Length of text to be displayed (in characters). If wrap is false, this is inferred as maximum length.
+ * @attribute {boolean} randomStart       - Whether baseText should pick a random word to start on.
+ * @attribute {boolean} wrap              - Whether baseText should return to the start if the length
  * condition hasn't been satisfied.
- * @property {string} lipsum             - Lorem Ipsum text. Used as a fallback if no user supplied
- * text.
  */
 // TODO: Allow child elements
-// TODO: Implement attributeChangedCallback
 export default class DjlLipsum extends HTMLElement {
   static define(tag = 'djl-lipsum') {
     customElements.define(tag, this)
   }
-
-  baseText; // string
-  length; // number|undefined
-  randomStart; // boolean
-  wrap // boolean
   
+  static get observedAttributes() {
+    return [ 'random-start', 'wrap', 'length' ]
+  }
 
-  lipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas '
-        + 'quis dui vel justo laoreet blandit. Cras tincidunt leo at mauris placerat '
-        + 'aliquam. Suspendisse potenti. Quisque a justo sed ipsum finibus sollicitudin. In '
-        + 'elementum sem ut nibh imperdiet, vitae varius eros consequat. Curabitur '
-        + 'facilisis a dui sit amet sodales. Fusce ante nisl, imperdiet sit amet tempus id, '
-        + 'hendrerit eu libero. Etiam laoreet luctus orci quis aliquam. Vivamus volutpat '
-        + 'fringilla mattis. Quisque et orci a enim consectetur aliquet ac sagittis augue. '
-        + 'Donec pretium orci quis justo mattis, quis molestie ex tristique. Quisque non '
-        + 'rhoncus eros, ut iaculis arcu.';
+  static {
+    // Declare getter/setter wrappers around observedAttributes (JS/DOM interface)
+    this.observedAttributes.forEach(attribute => {
+      Object.defineProperty(this.prototype, attribute, {
+        get: function() { return this.getAttribute(attribute) },
+        set: function(value) { return this.setAttribute(attribute, value) },
+      })
+    })
+  }
 
-  constructor() {
-    super();
+  static lipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas '
+  + 'quis dui vel justo laoreet blandit. Cras tincidunt leo at mauris placerat '
+  + 'aliquam. Suspendisse potenti. Quisque a justo sed ipsum finibus sollicitudin. In '
+  + 'elementum sem ut nibh imperdiet, vitae varius eros consequat. Curabitur '
+  + 'facilisis a dui sit amet sodales. Fusce ante nisl, imperdiet sit amet tempus id, '
+  + 'hendrerit eu libero. Etiam laoreet luctus orci quis aliquam. Vivamus volutpat '
+  + 'fringilla mattis. Quisque et orci a enim consectetur aliquet ac sagittis augue. '
+  + 'Donec pretium orci quis justo mattis, quis molestie ex tristique. Quisque non '
+  + 'rhoncus eros, ut iaculis arcu.';
 
-    this.baseText = this.textContent || this.lipsum;
-    this.textContent = this.baseText;
+  #baseText; // string
 
-    this.randomStart = (this.getAttribute('random-start') !== null);
+  #render() {
+    this.#baseText = this.textContent || DjlLipsum.lipsum;
+    this.textContent = this.#baseText;
+
     if (this.randomStart) {
       let words = this.textContent.split(" ");
       const startPosition = Math.floor(Math.random() * words.length) - 1;
@@ -47,24 +50,26 @@ export default class DjlLipsum extends HTMLElement {
       this.textContent = words.join(" ");
     }
 
-    this.wrap = (this.getAttribute('wrap') !== null);
-    this.length = !!this.getAttribute('length') ?
-        // NOTE: TypeScript doesn't like it, but 'length' will be 'string' here.
-        Number.parseInt(this.getAttribute('length') || '0') :
-        undefined;
+    const length = !!this.length
+        ? Number.parseInt(this.length)
+        : undefined;
 
-    if (this.length) {
-      if (this.textContent.length > this.length) {
-        this.textContent = ' ' + this.textContent.slice(0, this.length - 1);
+    if (length) {
+      if (this.textContent.length > length) {
+        this.textContent = ' ' + this.textContent.slice(0, length - 1);
       }
 
-      while (this.wrap && this.textContent.length < this.length) {
-        const extraCharsToGet = this.length - this.textContent.length;
-        const extraText = ' ' + this.baseText.slice(0, extraCharsToGet - 1);
+      while (this.wrap != null && this.textContent.length < length) {
+        const extraCharsToGet = length - this.textContent.length;
+        const extraText = ' ' + this.#baseText.slice(0, extraCharsToGet - 1);
         this.textContent += extraText;
       }
     }
   }
+
+  constructor() { super() }
+  attributeChangedCallback() { this.#render() }
+  connectedCallback() { this.#render() }
 }
 
 DjlLipsum.define()
